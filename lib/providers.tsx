1921779d7b154043857eddd87a94111dbed60609
Harley Jackson <harleyyesm@gmail.com>
2025-02-22 18:28:13 +0900

    first commit

---
+++ lib/providers.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import { DynamicContextProvider } from "@dynamic-labs/sdk-react-core";
+import { SolanaWalletConnectors } from "@dynamic-labs/solana";
+
+
+export default function Providers({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+
+  
+
+  return (
+    <DynamicContextProvider
+      theme="auto"
+      settings={{
+        environmentId: "2762a57b-faa4-41ce-9f16-abff9300e2c9",
+           walletConnectors: [SolanaWalletConnectors],
+      }}
+    >
+      {children}
+    </DynamicContextProvider>
+  );
+}
