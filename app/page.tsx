d831d2f3ac184f157a45ccfb82b7d274b4e0d153
Koki-T3 <reunion.fy0317@gmail.com>
2025-02-23 12:08:21 +0900

    save

--- app/page.tsx
+++ app/page.tsx
@@ -16,53 +16,54 @@ export default function CampaignPage() {
     const { user } = useAuth();
 
     if (!user) {
-        return <div>Loading user...</div>;
+      return <div>Loading user...</div>;
     }
 
     return (
-        <div>
-            <h1>User</h1>
-            <p>User ID: {user.userId}</p>
-            <p>Email: {user.email}</p>
-            <p>Phone Number: {user.phoneNumber}</p>
-            <p>Google Name: {user.google?.name}</p>
-            <p>Google Picture: {user.google?.picture}</p>
-            <p>Farcaster FID: {user.farcaster?.fid}</p>
-            <p>Farcaster Username: {user.farcaster?.username}</p>
-            <p>Farcaster Bio: {user.farcaster?.bio}</p>
-            <p>Farcaster Display Name: {user.farcaster?.displayName}</p>
-            <p>Farcaster PFP URL: {user.farcaster?.pfpUrl}</p>
-            <p>Farcaster Custody: {user.farcaster?.custody}</p>
-            <p>Farcaster Verifications: {user.farcaster?.verifications}</p>
-        </div>
+      <div>
+        <h1>User</h1>
+        {/* <p>User ID: {user.userId}</p>
+        <p>Email: {user.email}</p>
+        <p>Phone Number: {user.phoneNumber}</p>
+        <p>Google Name: {user.google?.name}</p>
+        <p>Google Picture: {user.google?.picture}</p> */}
+        <p>Farcaster FID: {user.farcaster?.fid}</p>
+        <p>Farcaster Username: {user.farcaster?.username}</p>
+        <p>Farcaster Bio: {user.farcaster?.bio}</p>
+        <p>Farcaster Display Name: {user.farcaster?.displayName}</p>
+        <p>Farcaster PFP URL: {user.farcaster?.pfpUrl}</p>
+        <p>Farcaster Custody: {user.farcaster?.custody}</p>
+        <p>Farcaster Verifications: {user.farcaster?.verifications}</p>
+      </div>
     );
-}
+  };
 
-const AuthButton =() => {
-  const { login, logout, user, jwt } = useAuth();
+  const AuthButton = () => {
+    const { login, logout, user, jwt } = useAuth();
 
-  return (
-    <div className="flex gap-4">
-      {user == null ? (
-        <button
-          type="button"
-          onClick={login}
-          className="bg-blue-500 text-white font-bold py-2 px-4 rounded"
-        >
-          Login
-        </button>
-      ) : (
-        <button
-          type="button"
-          onClick={logout}
-          className="bg-black text-white font-bold py-2 px-4 rounded border-2 border-blue-500"
-        >
-          Logout
-        </button>
-      )}
-      <User/>
-    </div>
-  )}
+    return (
+      <div className="flex gap-4">
+        {user == null ? (
+          <button
+            type="button"
+            onClick={login}
+            className="bg-blue-500 text-white font-bold py-2 px-4 rounded"
+          >
+            Login
+          </button>
+        ) : (
+          <button
+            type="button"
+            onClick={logout}
+            className="bg-black text-white font-bold py-2 px-4 rounded border-2 border-blue-500"
+          >
+            Logout
+          </button>
+        )}
+        <User />
+      </div>
+    );
+  };
 
   return (
     <div
@@ -207,6 +208,6 @@ const AuthButton =() => {
           </Button>
         </div>
       </section>
-      </div>
+    </div>
   );
 }
